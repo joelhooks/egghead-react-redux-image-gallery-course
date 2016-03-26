15283f76d2c1128b1bee881725399224f79b3755
joelhooks <joelhooks@gmail.com>
2016-03-26 13:38:32 -0500

    test the sagas

--- sagas.js
+++ sagas.js
@@ -1,11 +1,11 @@
 import {fetchImages} from './api';
 import {takeEvery} from 'redux-saga';
-import {put} from 'redux-saga/effects';
+import {put, call} from 'redux-saga/effects';
 
 import * as GalleryActions from './actions';
 
 export function* loadImages(action) {
-  const images = yield fetchImages(action.page);
+  const images = yield call(fetchImages, action.page);
   yield put(GalleryActions.imagesLoaded(images));
   yield put(GalleryActions.selectImage(images[0]));
 }
--- sagas.spec.js
+++ sagas.spec.js
@@ -1,7 +1,31 @@
 import test from 'tape';
+import { call, put } from 'redux-saga/effects';
+import {loadImages} from './sagas';
+import { fetchImages } from './api'
+import {imagesLoaded, selectImage} from './actions';
 
-test('is this even on?', assert => {
-  assert.equal(true, true, 'definitely not passing...');
+test('loadImages', assert => {
+  const generator = loadImages({page:1});
+
+  assert.deepEqual(
+    generator.next().value,
+    call(fetchImages, 1),
+    'should call fetchImages'
+  );
+
+  const images = ['test'];
+
+  assert.deepEqual(
+    generator.next(images).value,
+    put(imagesLoaded(images)),
+    'should dispatch IMAGES_LOADED action'
+  );
+
+  assert.deepEqual(
+    generator.next().value,
+    put(selectImage(images[0])),
+    'should dispatch SELECT_IMAGE action'
+  );
 
   assert.end();
 });
