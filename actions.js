287fe864f40a9de87c7c41a7947f7f533fc7d14b
joelhooks <joelhooks@gmail.com>
2016-03-26 11:24:35 -0500

    loading api data from flickr

--- actions.js
+++ actions.js
@@ -1,4 +1,12 @@
 export const SELECT_IMAGE = 'SELECT_IMAGE';
+export const IMAGES_LOADED = 'IMAGES_LOADED';
+
+export function imagesLoaded(images) {
+  return {
+    type: IMAGES_LOADED,
+    images
+  }
+}
 
 export function selectImage(selectedImage) {
   return {
