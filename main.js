97819e53f1effb872aba0e71f654cfe397ec57f5
joelhooks <joelhooks@gmail.com>
2016-03-26 10:13:55 -0500

    prep for lesson 03 cleanup

--- main.js
+++ main.js
@@ -10,19 +10,9 @@ import imagesReducer from './reducer';
 
 const store = createStore(imagesReducer);
 
-console.log(store);
-
-store.subscribe(() => {
-  console.log('subscribed:', store.getState());
-})
-
 store.dispatch({
   type: 'SELECT_IMAGE',
-  data: [1,2,3,4,5],
-  other: 'this is something too',
-  payload: {
-    info: 'some info'
-  }
+  image: '??'
 });
 
 ReactDom.render(
--- reducer.js
+++ reducer.js
@@ -3,12 +3,10 @@ const defaultState = {
 };
 
 export default function images(state = defaultState, action) {
-  console.log(state, action);
-  
   switch(action.type) {
     case 'SELECT_IMAGE':
       return {initialValue: state.initialValue + 2};
+    default:
+      return state;
   }
-
-  return state;
 }
